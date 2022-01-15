606ca76ba7f3f64103c95ddcdde90423c5eb295b
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-01-15 23:21:43 +0100

    Even/Odd numers

---
+++ EvenNum/src/main/java/EvenNumber.java
@@ -0,0 +1,9 @@
+public class EvenNumber {
+
+    public static void main(String[] args) {
+
+     System.out.println(EvenNumberCheck.isEven(21));
+     System.out.println(EvenNumberCheck.isOdd(21));
+
+    }
+}
---
+++ EvenNum/src/main/java/EvenNumberCheck.java
@@ -0,0 +1,13 @@
+public class EvenNumberCheck {
+
+    public static boolean isEven (int checkNumber){
+
+        return  (checkNumber % 2) == 0;
+    }
+
+    public static boolean isOdd (int checkNumber){
+
+        return  (checkNumber % 2) != 0;
+    }
+
+}
