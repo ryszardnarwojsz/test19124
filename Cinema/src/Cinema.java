6b1d6372fe71deb36e67baab6d796e45564d6dde
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-01-16 14:59:17 +0100

    Cinema commits

---
+++ Cinema/src/Cinema.java
@@ -0,0 +1,13 @@
+public class Cinema {
+
+    public static void main(String[] args) {
+
+    CinemaPlace newCinemaPlace1 = new CinemaPlace(1,33);
+
+        System.out.println(newCinemaPlace1);
+        newCinemaPlace1.setOccupied(true);
+        System.out.println(newCinemaPlace1);
+
+
+    }
+}
