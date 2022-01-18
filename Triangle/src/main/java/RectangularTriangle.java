4c527409d6422ce6a78c57452f43415a87811c28
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-01-18 23:08:37 +0100

    Triangle commits

---
+++ Triangle/src/main/java/RectangularTriangle.java
@@ -0,0 +1,8 @@
+public class RectangularTriangle {
+
+    public static boolean ifRectangularTriangle(float a ,float b,float c){
+
+                return (float) Math.pow(a,2) + (float) Math.pow(b,2) == (float) Math.pow(c,2);
+      }
+
+}
