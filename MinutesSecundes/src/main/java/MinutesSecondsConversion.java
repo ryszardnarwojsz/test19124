7238f6e67a9ad1bc5b2b33a04e4d20a38036c690
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-01-18 21:15:41 +0100

    Minutes & Seconds commits

---
+++ MinutesSecundes/src/main/java/MinutesSecondsConversion.java
@@ -0,0 +1,10 @@
+public class MinutesSecondsConversion {
+
+    private static double conversion;
+
+    public static double conversionMinutesIntoSeconds(int minutes){
+        conversion = (double) minutes * 60;
+        return conversion;
+    }
+
+}
