0e070443492485dc112534a255ac659c1ce87b27
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-01-19 21:37:58 +0100

    Minutes & seconds correction

--- MinutesSecundes/src/main/java/MinutesSecondsConversion.java
+++ MinutesSecundes/src/main/java/MinutesSecondsConversion.java
@@ -1,10 +1,8 @@
 public class MinutesSecondsConversion {
 
-    private static double conversion;
 
     public static double conversionMinutesIntoSeconds(int minutes){
-        conversion = (double) minutes * 60;
-        return conversion;
+        return (long) minutes * 60;
     }
 
 }
