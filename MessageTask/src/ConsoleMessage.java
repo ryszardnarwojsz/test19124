93265576a6c95891372be0115f8fca63afcc5e8b
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-01-20 23:44:00 +0100

    Message commits

---
+++ MessageTask/src/ConsoleMessage.java
@@ -0,0 +1,8 @@
+public class ConsoleMessage extends Message {
+
+    @Override
+    public String getMessage() {
+        return "This is class " + this.getClass().getName();
+    }
+
+}
---
+++ MessageTask/src/WindowMessage.java
@@ -0,0 +1,8 @@
+public class WindowMessage extends Message{
+
+    @Override
+    public String getMessage() {
+        return "This is class " + this.getClass().getName();
+    }
+
+}
