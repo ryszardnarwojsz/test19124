775bea96afd30330248433a9cb2e5c9c86a071d6
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-01-24 23:59:32 +0100

    Cinema employee commits correction - Capital letters

--- CinemaEmployee/src/TicketSeller.java
+++ CinemaEmployee/src/TicketSeller.java
@@ -1,9 +1,9 @@
 public class TicketSeller extends BaseEmployee{
 
-    private static final String employeePosition = "TicketSeller";
+    private static final String EMPLOYEE_POSITION = "TicketSeller";
 
     public TicketSeller(String employeeName, String employeeSurname, int hiredDate) {
-        super(employeeName, employeeSurname,employeePosition, hiredDate);
+        super(employeeName, employeeSurname,EMPLOYEE_POSITION, hiredDate);
     }
 
     @Override
