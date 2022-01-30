0126d6e1640d1cf231688a41072554a9eb3dc259
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-01-30 22:00:54 +0100

    Search for word in data.txt commit

---
+++ FileTXTwordSearch/src/FileTxtReader.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
+
+public class FileTxtReader {
+
+
+    public boolean containsSelectedWord(String pathTofile, String word) throws IOException {
+
+        String text;
+        boolean isContainWord = false;
+
+        word = word.toLowerCase();
+
+        try(BufferedReader newBufferedReader = new BufferedReader(new FileReader(pathTofile))){
+
+            while ((text = newBufferedReader.readLine()) != null ) {
+                text = text.toLowerCase();
+
+                if(text.contains(word)){
+                isContainWord = text.contains(word);
+                }
+            }
+            return isContainWord;
+        }
+
+    }
+}
