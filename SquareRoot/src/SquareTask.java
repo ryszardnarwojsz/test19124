0681714dd5baa0b1b8d88d3c7441b30ab0d991ba
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-02-02 23:07:20 +0100

    SquareRoot commits

---
+++ SquareRoot/src/SquareTask.java
@@ -0,0 +1,8 @@
+public class SquareTask {
+
+    public static void main(String[] args) {
+        SquareRoot newSquareRoot = new SquareRoot();
+        System.out.println(newSquareRoot.squareCalculate(-1));
+
+    }
+}
