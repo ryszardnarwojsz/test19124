e90e6b42ae8b2d50adea8729237e1ab7a4e17d45
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-02-10 21:09:59 +0100

    URL validate commit correction

--- URLvaildate/src/Url.java
+++ URLvaildate/src/Url.java
@@ -3,9 +3,10 @@ import java.util.regex.Pattern;
 
 public class Url {
 
+    private final static Pattern pattern = Pattern.compile("http(s|)://\\D{4,14}\\.(com|pl)");
+
     public static boolean urlCheck(String url){
 
-        Pattern pattern = Pattern.compile("http(s|)://\\D{4,14}\\.(com|pl)");
         Matcher matcher = pattern.matcher(url);
 
         return matcher.matches();
