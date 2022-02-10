07dbfe5c148ee0325de8ce5c96817d6825193788
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-02-10 21:04:57 +0100

    Pesel validate commit correction

--- peselValidate/src/Pesel.java
+++ peselValidate/src/Pesel.java
@@ -3,9 +3,10 @@ import java.util.regex.Pattern;
 
 public class Pesel {
 
-    public static boolean peselCheck(String pesel){
+    private static final Pattern pattern = Pattern.compile("\\d{11}");
 
-        Pattern pattern = Pattern.compile("\\d{11}");
+    public static boolean peselCheck(String pesel) {
+
         Matcher matcher = pattern.matcher(pesel);
 
         return matcher.matches();
