67e5d3a4d6817cb91104de474213ef2f63992f23
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-02-12 14:54:47 +0100

    Date of birth commit

--- DateOfBirth/src/DateOfBirthTask.java
+++ DateOfBirth/src/DateOfBirthTask.java
@@ -16,9 +16,9 @@ public class DateOfBirthTask {
         LocalDate parseDateOfBirth = LocalDate.parse(dateOfBirth);
         Period newPeriod = Period.between(parseDateOfBirth, LocalDate.now());
 
-        System.out.println("Person is "+ newPeriod.getYears() +" years old");
-        System.out.println("Person was born on "+ parseDateOfBirth.getDayOfWeek());
-        System.out.println("Person was born in week: "+ parseDateOfBirth.get(ChronoField.ALIGNED_WEEK_OF_YEAR));
+        System.out.println("Person is " + newPeriod.getYears() + " years old");
+        System.out.println("Person was born on " + parseDateOfBirth.getDayOfWeek());
+        System.out.println("Person was born in week: " + parseDateOfBirth.get(ChronoField.ALIGNED_WEEK_OF_YEAR));
 
     }
 }
