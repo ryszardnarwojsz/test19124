4c04ad9750eea1dfd3c95086bd81994545c1dbf9
Ryszard Narwojsz <ryszardnarwojsz@gmail.com>
2022-02-12 12:32:56 +0100

    Password commits

---
+++ Password/src/PasswordTask.java
@@ -0,0 +1,9 @@
+public class PasswordTask {
+
+    public static void main(String[] args) {
+
+        System.out.println(Password.validatePassword("sdfsdfZdff6"));
+        System.out.println(Password.validatePassword("s2021fsdfZdff6"));
+    }
+
+}
